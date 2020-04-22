21de784a1f280ab1dbf7ba5970e33707890ade2e
Mattia De Prisco <mattia.dipi@gmail.com>
2020-04-22 17:45:55 +0200

    Create actions

--- src/actions.hpp
+++ src/actions.hpp
@@ -2,18 +2,26 @@
 #define _ACTIONS_HPP_
 
 #include "message_queue.hpp"
+#include <memory>
+#include "fmod.hpp"
 
-enum class ActionType
+class Action
 {
-    Pause,
-    Unpause,
-    RaiseVolume,
-    LowerVolume,
-    Stop,
-    Quit
+    public:
+        virtual void execute(FMOD::Channel* channel) = 0;
 };
 
-class ActionQueue : public MessageQueue<ActionType>
+class PlayPause : public Action
+{
+    virtual void execute(FMOD::Channel* channel)
+    { 
+        bool paused;
+        channel->getPaused(&paused);
+        paused ? channel->setPaused(false) : channel->setPaused(true);
+    }
+};
+
+class ActionQueue : public MessageQueue<std::unique_ptr<Action>>
 {
     public:
         bool AreActionsRequested();
