eac71d17bfccde582d1096471e80dec413ccf55f
Mattia De Prisco <mattia.dipi@gmail.com>
2020-04-23 14:24:55 +0200

    Update message when not able to play an audio

--- src/music_player.cpp
+++ src/music_player.cpp
@@ -75,7 +75,10 @@ void MusicPlayer::run()
         catch(const std::exception& e)
         {
             std::cout << e.what() << std::endl;
-            std::cout << "Requested file does not exists or it is not supported." << std::endl;
+            std::cout << "Possible causes: << std::endl;
+            std::cout << "  - requested file does not exist" << std::endl;
+            std::cout << "  - requested file is not an audio file" << std::endl;
+            std::cout << "  - your system has problems with sound drivers" << std::endl;
         }
         sound->release();
         current_audio_.reset();
